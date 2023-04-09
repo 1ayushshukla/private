5a2f9b98e3742fdb8d427b0ec30a5d0962f55e19
Ayush Shukla <you@example.com>
2023-04-09 22:14:26 +0530

    Added functionality to calculate distance

--- app/src/main/java/com/example/map3/MainActivity.kt
+++ app/src/main/java/com/example/map3/MainActivity.kt
@@ -9,6 +9,9 @@ import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -22,6 +25,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
@@ -29,13 +33,15 @@ import java.io.IOException
 import java.util.*
 import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraMoveStartedListener, GoogleMap.OnCameraIdleListener{
+class MainActivity : AppCompatActivity() , OnMapReadyCallback{
+    //, LocationListener, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraMoveStartedListener, GoogleMap.OnCameraIdleListener
 
     private var mMap: GoogleMap? = null
     lateinit var mapView: MapView
     private var MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
 
     lateinit var tvCurrentAddress: TextView
+    lateinit var b_search: Button
 
 
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
@@ -46,6 +52,13 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
     // ZOOM LEVELS - 1 - World, 5 - Landmass/continent, 10 - City, 15 - Streets, 20 - Buildings
     private val DEFAULT_ZOOM = 15f
     private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+
+    var end_latitude = 0.0
+    var end_longitude = 0.0
+    var origin: MarkerOptions? = null
+    var destination: MarkerOptions? = null
+    var latitude = 0.0
+    var longitude = 0.0
 
 
     override fun onMapReady(p0: GoogleMap) {
@@ -65,9 +78,9 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
             return
         }
         mMap!!.isMyLocationEnabled
-        mMap!!.setOnCameraMoveListener(this)
-        mMap!!.setOnCameraMoveStartedListener(this)
-        mMap!!.setOnCameraIdleListener(this)
+//        mMap!!.setOnCameraMoveListener(this)
+//        mMap!!.setOnCameraMoveStartedListener(this)
+//        mMap!!.setOnCameraIdleListener(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,6 +89,7 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
 
         mapView = findViewById<MapView>(R.id.map1)
         tvCurrentAddress = findViewById<TextView>(R.id.tvAdd)
+        b_search = findViewById<Button>(R.id.B_search)
 
         //-------------------------PERMISSION BLOCK BEGIN--------------------------//
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
@@ -91,6 +105,64 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
         }
         mapView.onCreate(mapViewBundle)
         mapView.getMapAsync(this)
+
+        b_search.setOnClickListener {
+            searchArea()
+        }
+
+    }
+
+    private fun searchArea() {
+        val tf_location = findViewById<EditText>(R.id.TF_location)
+
+        val location = tf_location.text.toString()
+        var addressList: List<Address>? = null
+        val markerOptions = MarkerOptions()
+
+        if(location != null){
+            val geocoder = Geocoder(applicationContext)
+            try{
+                addressList = geocoder.getFromLocationName(location, 5)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            if(addressList != null){
+                for(i in addressList.indices){
+                    val myAddress = addressList[i]
+                    val latLng = LatLng(myAddress.latitude, myAddress.longitude)
+                    markerOptions.position(latLng)
+                    mMap!!.addMarker(markerOptions)
+
+                    end_latitude = myAddress.latitude
+                    end_longitude = myAddress.longitude
+
+                    mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+
+                    val mo = markerOptions
+                    mo.title("Distance")
+
+                    //for the searchArea() to calculate distance, get the lat long from getCurrentLocation() function
+                    //latitude = currentLocation.latitude
+                    //longitude = currentLocation.longitude
+
+                    val results = FloatArray(10)
+                    Location.distanceBetween(latitude,longitude,end_latitude,end_longitude,results)
+                    // for conversion to kms
+                    val s = String.format("%.1f", results[0]/1000)
+
+                    // Setting marker to draw route between these 2 points
+                    origin = MarkerOptions().position(LatLng(latitude, longitude)).title("HSR Layout").snippet("origin")
+                    destination = MarkerOptions().position(LatLng(end_latitude, end_longitude)).title(tf_location.text.toString()).snippet("Distance - $s KM")
+
+                    mMap!!.addMarker(destination!!)
+                    mMap!!.addMarker(origin!!)
+
+                    Toast.makeText(applicationContext, "Distance - $s KM", Toast.LENGTH_SHORT).show()
+
+                    tvCurrentAddress!!.setText("Distance - $s KM")
+                }
+            }
+        }
 
     }
 
@@ -160,6 +232,10 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
                             Toast.makeText(this@MainActivity, "Current Location Found.", Toast.LENGTH_SHORT).show()
                             //call the moveCamera() function
                             moveCamera(LatLng(currentLocation.latitude, currentLocation.longitude), DEFAULT_ZOOM)
+
+                            //for the searchArea() to calculate distance
+                            latitude = currentLocation.latitude
+                            longitude = currentLocation.longitude
                         }
                     }else {
                         Toast.makeText(this@MainActivity, "Current Location Not Found.", Toast.LENGTH_SHORT).show()
@@ -184,48 +260,48 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener,
 
 
     //-------------------------ADDRESS FUNCTIONALITY BEGIN--------------------------//
-    override fun onLocationChanged(location: Location) {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        var address: List<Address>? = null
-        try {
-            address = geocoder.getFromLocation(location.latitude, location.longitude,1)
-        }catch (e: IOException){
-            e.printStackTrace()
-        }
-        setAddress(address!![0])
-    }
-
-    private fun setAddress(address: Address) {
-        if(address != null){
-            if(address.getAddressLine(0) != null){
-                tvCurrentAddress!!.text = address.getAddressLine(0)
-            }
-            if(address.getAddressLine(1) != null){
-                tvCurrentAddress!!.text = tvCurrentAddress.text.toString() + address.getAddressLine(1).toString()
-            }
-        }
-    }
-
-    override fun onCameraMove() {
-
-    }
-
-    override fun onCameraMoveStarted(p0: Int) {
-
-    }
-
-    override fun onCameraIdle() {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        var address: List<Address>? = null
-        try {
-            address = geocoder.getFromLocation(mMap!!.cameraPosition.target.latitude, mMap!!.cameraPosition.target.longitude, 1)
-            setAddress(address!![0])
-        }catch (e: java.lang.IndexOutOfBoundsException){
-            e.printStackTrace()
-        }catch (e: IOException){
-            e.printStackTrace()
-        }
-    }
+//    override fun onLocationChanged(location: Location) {
+//        val geocoder = Geocoder(this, Locale.getDefault())
+//        var address: List<Address>? = null
+//        try {
+//            address = geocoder.getFromLocation(location.latitude, location.longitude,1)
+//        }catch (e: IOException){
+//            e.printStackTrace()
+//        }
+//        setAddress(address!![0])
+//    }
+//
+//    private fun setAddress(address: Address) {
+//        if(address != null){
+//            if(address.getAddressLine(0) != null){
+//                tvCurrentAddress!!.text = address.getAddressLine(0)
+//            }
+//            if(address.getAddressLine(1) != null){
+//                tvCurrentAddress!!.text = tvCurrentAddress.text.toString() + address.getAddressLine(1).toString()
+//            }
+//        }
+//    }
+//
+//    override fun onCameraMove() {
+//
+//    }
+//
+//    override fun onCameraMoveStarted(p0: Int) {
+//
+//    }
+//
+//    override fun onCameraIdle() {
+//        val geocoder = Geocoder(this, Locale.getDefault())
+//        var address: List<Address>? = null
+//        try {
+//            address = geocoder.getFromLocation(mMap!!.cameraPosition.target.latitude, mMap!!.cameraPosition.target.longitude, 1)
+//            setAddress(address!![0])
+//        }catch (e: java.lang.IndexOutOfBoundsException){
+//            e.printStackTrace()
+//        }catch (e: IOException){
+//            e.printStackTrace()
+//        }
+//    }
 
     //-------------------------ADDRESS FUNCTIONALITY END--------------------------//
 
